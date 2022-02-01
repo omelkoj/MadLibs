0475c00abc942d1dee869c755cada0ef0f10ce20
JULIA OMELKO <omelkoj@gmail.com>
2022-01-31 21:57:54 -0500

    Commit 1

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+#youtuber = "Julia"
+
+#print("subscribe to " + youtuber)
+#print("subscribe to {}".format(youtuber))
+#print(f"subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"I feel so {adj}! I just want to {verb1} all of the time. In some ways, {verb2} like you are {famous_person}!"
+
+print(madlib)
